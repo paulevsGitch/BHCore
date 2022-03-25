b6f0ac035e7ae64facef803ecc51d09a8328ebc1
paulevsGitch <paulevs@yandex.ru>
2022-03-25 17:04:10 +0300

    Enum array enhancements

--- src/main/java/paulevs/bhcore/storage/EnumArraySectionData.java
+++ src/main/java/paulevs/bhcore/storage/EnumArraySectionData.java
@@ -12,8 +12,27 @@ public class EnumArraySectionData <E extends Enum<E>> implements CustomSectionDa
 	
 	public EnumArraySectionData(int capacity, Class<E> enumClass) {
 		constants = enumClass.getEnumConstants();
-		type = constants.length < 16 ? ArrayType.NIBBLE : constants.length < 256 ? ArrayType.BYTE : ArrayType.INT;
+		type = constants.length < ArrayType.NIBBLE.maxValue ? ArrayType.NIBBLE : constants.length < ArrayType.BYTE.maxValue ? ArrayType.BYTE : ArrayType.INT;
 		array = type.constructor.apply(capacity);
+		
+		if (type == ArrayType.NIBBLE) {
+			NibbleArraySectionData data = NibbleArraySectionData.class.cast(array);
+			for (int i = 0; i < capacity; i++) {
+				data.setData(i, (byte) type.maxValue);
+			}
+		}
+		else if (type == ArrayType.BYTE) {
+			ByteArraySectionData data = ByteArraySectionData.class.cast(array);
+			for (int i = 0; i < capacity; i++) {
+				data.setData(i, (byte) type.maxValue);
+			}
+		}
+		else {
+			IntArraySectionData data = IntArraySectionData.class.cast(array);
+			for (int i = 0; i < capacity; i++) {
+				data.setData(i, type.maxValue);
+			}
+		}
 	}
 	
 	@Override
@@ -29,11 +48,11 @@ public class EnumArraySectionData <E extends Enum<E>> implements CustomSectionDa
 	public E getData(int index) {
 		int value = -1;
 		switch (type) {
-			case NIBBLE -> value = NibbleArraySectionData.class.cast(array).getData(index);
-			case BYTE -> value = ByteArraySectionData.class.cast(array).getData(index);
-			case INT -> value = IntArraySectionData.class.cast(array).getData(index);
+			case NIBBLE -> value = NibbleArraySectionData.class.cast(array).getData(index) & type.maxValue;
+			case BYTE -> value = ByteArraySectionData.class.cast(array).getData(index) & type.maxValue;
+			case INT -> value = IntArraySectionData.class.cast(array).getData(index) & type.maxValue;
 		}
-		if (value == -1) return null;
+		if (value == type.maxValue) return null;
 		return constants[value];
 	}
 	
@@ -48,20 +67,18 @@ public class EnumArraySectionData <E extends Enum<E>> implements CustomSectionDa
 	}
 	
 	private enum ArrayType {
-		NIBBLE(0, NibbleArraySectionData::new),
-		BYTE(1, ByteArraySectionData::new),
-		INT(2, IntArraySectionData::new);
+		NIBBLE(0, NibbleArraySectionData::new, 15),
+		BYTE(1, ByteArraySectionData::new, 255),
+		INT(2, IntArraySectionData::new, Integer.MAX_VALUE);
 		
 		final Function<Integer, CustomSectionData> constructor;
+		final int maxValue;
 		final byte type;
 		
-		ArrayType(int type, Function<Integer, CustomSectionData> constructor) {
+		ArrayType(int type, Function<Integer, CustomSectionData> constructor, int maxValue) {
 			this.constructor = constructor;
+			this.maxValue = maxValue;
 			this.type = (byte) type;
-		}
-		
-		int getIndex(int value) {
-			return this == NIBBLE ? value & 15 : this == BYTE ? value & 255 : value;
 		}
 	}
 }
