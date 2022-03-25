a66d6cb6f759906c7ae6f2a697e9fdcdd3d0b11d
paulevsGitch <paulevs@yandex.ru>
2022-03-25 19:51:23 +0300

    Small name change

--- src/main/java/paulevs/bhcore/mixin/ChunkSectionMixin.java
+++ src/main/java/paulevs/bhcore/mixin/ChunkSectionMixin.java
@@ -15,13 +15,13 @@ import paulevs.bhcore.storage.SectionDataHandler;
 
 @Mixin(value = ChunkSection.class, remap = false)
 public class ChunkSectionMixin implements CoreChunkSection {
-	private CustomSectionData[] data;
+	@Unique private CustomSectionData[] bhc_sectionData;
 	
 	@Inject(method = "<init>(SSSS)V", at = @At("TAIL"))
 	private void bhc_onChunkSectionInit(short yOffset, short nonEmptyBlockCount, short randomTickableBlockCount, short nonEmptyFluidCount, CallbackInfo info) {
-		data = new CustomSectionData[SectionDataHandler.getCount()];
-		for (int i = 0; i < data.length; i++) {
-			data[i] = SectionDataHandler.getConstructor(i).get();
+		bhc_sectionData = new CustomSectionData[SectionDataHandler.getCount()];
+		for (int i = 0; i < bhc_sectionData.length; i++) {
+			bhc_sectionData[i] = SectionDataHandler.getConstructor(i).get();
 		}
 	}
 	
@@ -30,23 +30,23 @@ public class ChunkSectionMixin implements CoreChunkSection {
 		target = "Lnet/modificationstation/stationapi/impl/level/chunk/PalettedContainer;write(Lnet/minecraft/util/io/CompoundTag;Ljava/lang/String;Ljava/lang/String;)V"
 	), locals = LocalCapture.CAPTURE_FAILHARD)
 	public void bhc_onTagSave(CallbackInfoReturnable<CompoundTag> info, CompoundTag tag) {
-		for (int index = 0; index < data.length; index++) {
+		for (int index = 0; index < bhc_sectionData.length; index++) {
 			String key = SectionDataHandler.getKey(index);
-			data[index].saveToBNT(key, tag);
+			bhc_sectionData[index].saveToBNT(key, tag);
 		}
 	}
 	
 	@Inject(method = "fromNBT(Lnet/minecraft/util/io/CompoundTag;Lnet/minecraft/util/io/CompoundTag;)V", at = @At("HEAD"))
 	public void bhc_onTagLoad(CompoundTag chunkTag, CompoundTag sectionTag, CallbackInfo info) {
-		for (int index = 0; index < data.length; index++) {
+		for (int index = 0; index < bhc_sectionData.length; index++) {
 			String key = SectionDataHandler.getKey(index);
-			data[index].loadFromBNT(key, sectionTag);
+			bhc_sectionData[index].loadFromBNT(key, sectionTag);
 		}
 	}
 	
 	@Unique
 	@Override
 	public CustomSectionData bhc_getData(int index) {
-		return data[index];
+		return bhc_sectionData[index];
 	}
 }
