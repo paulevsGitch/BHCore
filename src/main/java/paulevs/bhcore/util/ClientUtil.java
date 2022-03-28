8b0750b4b022a7af248827ad2891aaffb99eb576
paulevsGitch <paulevs@yandex.ru>
2022-03-28 12:13:31 +0300

    More client functions, small array fix

--- src/main/java/paulevs/bhcore/util/ClientUtil.java
+++ src/main/java/paulevs/bhcore/util/ClientUtil.java
@@ -34,4 +34,29 @@ public class ClientUtil {
 	public static boolean isFancyGraphics() {
 		return getMinecraft().options.fancyGraphics;
 	}
+	
+	/**
+	 * Update area on client side.
+	 * @param level client {@link Level}.
+	 * @param x1 start X coordinate.
+	 * @param y1 start Y coordinate.
+	 * @param z1 start Z coordinate.
+	 * @param x2 end X coordinate.
+	 * @param y2 end Y coordinate.
+	 * @param z2 end Z coordinate.
+	 */
+	public static void updateArea(Level level, int x1, int y1, int z1, int x2, int y2, int z2) {
+		level.method_202(x1, y1, z1, x2, y2, z2);
+	}
+	
+	/**
+	 * Update block on client side.
+	 * @param level client {@link Level}.
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 * @param z Z coordinate.
+	 */
+	public static void updateBlock(Level level, int x, int y, int z) {
+		updateArea(level, x, y, z, x, y, z);
+	}
 }
