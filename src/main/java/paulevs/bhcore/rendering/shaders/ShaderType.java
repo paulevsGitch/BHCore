1652730e3bf9e97320eb9f94d7b4ed586665df71
paulevsGitch <paulevs@yandex.ru>
2022-04-05 15:43:13 +0300

    Geometry shader type

--- src/main/java/paulevs/bhcore/rendering/shaders/ShaderType.java
+++ src/main/java/paulevs/bhcore/rendering/shaders/ShaderType.java
@@ -3,10 +3,12 @@ package paulevs.bhcore.rendering.shaders;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL32;
 
 @Environment(EnvType.CLIENT)
 public enum ShaderType {
 	VERTEX(GL20.GL_VERTEX_SHADER),
+	GEOMETRY(GL32.GL_GEOMETRY_SHADER),
 	FRAGMENT(GL20.GL_FRAGMENT_SHADER);
 	
 	private final int id;
