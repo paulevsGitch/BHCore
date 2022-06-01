d9b9a27836bf1d1fcae317cc907ff06a86ce2ee7
paulevsGitch <paulevs@yandex.ru>
2022-06-01 14:25:04 +0300

    Utility world arg update

--- src/main/java/paulevs/bhcore/util/BlocksUtil.java
+++ src/main/java/paulevs/bhcore/util/BlocksUtil.java
@@ -1,6 +1,8 @@
 package paulevs.bhcore.util;
 
+import net.minecraft.level.BlockView;
 import net.minecraft.level.Level;
+import net.minecraft.level.WorldPopulationRegion;
 import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.level.BlockStateView;
 import net.modificationstation.stationapi.api.util.math.Direction;
@@ -11,45 +13,45 @@ public class BlocksUtil {
 	
 	/**
 	 * Set block state in world.
-	 * @param level {@link Level}
+	 * @param level {@link BlockView}, {@link Level} or {@link WorldPopulationRegion}
 	 * @param pos {@link Vec3I} block coordinate
 	 * @param state {@link BlockState} to place
 	 */
-	public static void setBlockState(Level level, Vec3I pos, BlockState state) {
+	public static void setBlockState(BlockView level, Vec3I pos, BlockState state) {
 		setBlockState(level, pos.x, pos.y, pos.z, state);
 	}
 	
 	/**
 	 * Set block state in world.
-	 * @param level {@link Level}
+	 * @param level {@link BlockView}, {@link Level} or {@link WorldPopulationRegion}
 	 * @param x X block coordinate
 	 * @param y Y block coordinate
 	 * @param z Z block coordinate
 	 * @param state {@link BlockState} to place
 	 */
-	public static void setBlockState(Level level, int x, int y, int z, BlockState state) {
+	public static void setBlockState(BlockView level, int x, int y, int z, BlockState state) {
 		((BlockStateView) level).setBlockState(x, y, z, state);
 	}
 	
 	/**
 	 * Get block state from world.
-	 * @param level {@link Level}
+	 * @param level {@link BlockView}, {@link Level} or {@link WorldPopulationRegion}
 	 * @param pos {@link Vec3I} block coordinate
 	 * @return {@link BlockState} at this position
 	 */
-	public static BlockState getBlockState(Level level, Vec3I pos) {
+	public static BlockState getBlockState(BlockView level, Vec3I pos) {
 		return getBlockState(level, pos.x, pos.y, pos.z);
 	}
 	
 	/**
 	 * Get block state from world.
-	 * @param level {@link Level}
+	 * @param level {@link BlockView}, {@link Level} or {@link WorldPopulationRegion}
 	 * @param x X block coordinate
 	 * @param y Y block coordinate
 	 * @param z Z block coordinate
 	 * @return {@link BlockState} at this position
 	 */
-	public static BlockState getBlockState(Level level, int x, int y, int z) {
+	public static BlockState getBlockState(BlockView level, int x, int y, int z) {
 		return ((BlockStateView) level).getBlockState(x, y, z);
 	}
 	
