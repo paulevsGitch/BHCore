c7e05d07097deb36049ac3b9252e456ec914347c
paulevsGitch <paulevs@yandex.ru>
2022-06-01 13:40:58 +0300

    More block functions

--- src/main/java/paulevs/bhcore/util/BlocksUtil.java
+++ src/main/java/paulevs/bhcore/util/BlocksUtil.java
@@ -3,13 +3,62 @@ package paulevs.bhcore.util;
 import net.minecraft.level.Level;
 import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.level.BlockStateView;
+import net.modificationstation.stationapi.api.util.math.Direction;
+import paulevs.bhcore.storage.vector.Vec3I;
 
 public class BlocksUtil {
+	private static final Direction[] DIRECTIONS = Direction.values();
+	
+	/**
+	 * Set block state in world.
+	 * @param level {@link Level}
+	 * @param pos {@link Vec3I} block coordinate
+	 * @param state {@link BlockState} to place
+	 */
+	public static void setBlockState(Level level, Vec3I pos, BlockState state) {
+		setBlockState(level, pos.x, pos.y, pos.z, state);
+	}
+	
+	/**
+	 * Set block state in world.
+	 * @param level {@link Level}
+	 * @param x X block coordinate
+	 * @param y Y block coordinate
+	 * @param z Z block coordinate
+	 * @param state {@link BlockState} to place
+	 */
 	public static void setBlockState(Level level, int x, int y, int z, BlockState state) {
 		((BlockStateView) level).setBlockState(x, y, z, state);
 	}
 	
+	/**
+	 * Get block state from world.
+	 * @param level {@link Level}
+	 * @param pos {@link Vec3I} block coordinate
+	 * @return {@link BlockState} at this position
+	 */
+	public static BlockState getBlockState(Level level, Vec3I pos) {
+		return getBlockState(level, pos.x, pos.y, pos.z);
+	}
+	
+	/**
+	 * Get block state from world.
+	 * @param level {@link Level}
+	 * @param x X block coordinate
+	 * @param y Y block coordinate
+	 * @param z Z block coordinate
+	 * @return {@link BlockState} at this position
+	 */
 	public static BlockState getBlockState(Level level, int x, int y, int z) {
 		return ((BlockStateView) level).getBlockState(x, y, z);
+	}
+	
+	/**
+	 * Get direction from block face integer. Can be used to correctly update state of placed blocks
+	 * @param facing block facing [0-5]
+	 * @return {@link Direction} that matches facing
+	 */
+	public static Direction fromFacing(int facing) {
+		return DIRECTIONS[facing];
 	}
 }
