cf2ea825f67905e1f5dfc3814f985c7996a0cbd9
paulevsGitch <paulevs@yandex.ru>
2022-06-03 18:19:22 +0300

    Tool tag check methods

--- src/main/java/paulevs/bhcore/util/ToolsUtil.java
+++ src/main/java/paulevs/bhcore/util/ToolsUtil.java
@@ -1,8 +1,10 @@
 package paulevs.bhcore.util;
 
 import net.minecraft.block.BlockBase;
+import net.minecraft.item.ItemInstance;
 import net.modificationstation.stationapi.api.BlockToolLogic;
 import net.modificationstation.stationapi.api.registry.Identifier;
+import net.modificationstation.stationapi.api.tags.TagRegistry;
 
 public class ToolsUtil {
 	public static final Identifier AXE = Identifier.of("tools/axes");
@@ -64,5 +66,60 @@ public class ToolsUtil {
 	 */
 	public static void setShears(BlockBase block, int level) {
 		setTool(block, SHEARS, level);
+	}
+	
+	/**
+	 * Check if item has tag.
+	 * @param tag {@link Identifier} tool tag.
+	 * @param item {@link ItemInstance} item.
+	 * @return true if item has tag
+	 */
+	public static boolean hasTag(Identifier tag, ItemInstance item) {
+		return TagRegistry.INSTANCE.tagMatches(tag, item);
+	}
+	
+	/**
+	 * Check if item has axe tag.
+	 * @param item {@link ItemInstance} item.
+	 * @return true if item is axe
+	 */
+	public static boolean isAxe(ItemInstance item) {
+		return TagRegistry.INSTANCE.tagMatches(AXE, item);
+	}
+	
+	/**
+	 * Check if item has pickaxe tag.
+	 * @param item {@link ItemInstance} item.
+	 * @return true if item is pickaxe
+	 */
+	public static boolean isPickaxe(ItemInstance item) {
+		return TagRegistry.INSTANCE.tagMatches(PICKAXE, item);
+	}
+	
+	/**
+	 * Check if item has shovel tag.
+	 * @param item {@link ItemInstance} item.
+	 * @return true if item is shovel
+	 */
+	public static boolean isShovel(ItemInstance item) {
+		return TagRegistry.INSTANCE.tagMatches(SHOVEL, item);
+	}
+	
+	/**
+	 * Check if item has sword tag.
+	 * @param item {@link ItemInstance} item.
+	 * @return true if item is sword
+	 */
+	public static boolean isSword(ItemInstance item) {
+		return TagRegistry.INSTANCE.tagMatches(SWORD, item);
+	}
+	
+	/**
+	 * Check if item has shears tag.
+	 * @param item {@link ItemInstance} item.
+	 * @return true if item is shears
+	 */
+	public static boolean isShears(ItemInstance item) {
+		return TagRegistry.INSTANCE.tagMatches(SHEARS, item);
 	}
 }
